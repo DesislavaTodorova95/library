456634dae43888f2c077047d07c84936709c36a3
Desislava Todorova <desi_14_94@abv.bg>
2022-01-30 17:01:05 +0200

    search field

--- src/components/Catalog/CatalogComponent.js
+++ src/components/Catalog/CatalogComponent.js
@@ -1,11 +1,21 @@
 import "./CatalogComponent.css";
 import NavComponent from "../NavComponent/NavComponent";
+
 const CatalogComponent = () => {
   return (
     <>
       <NavComponent />
       <div id="catalogDiv">
         <p className="headCatalog">All books</p>
+        <form id="searchBar">
+          
+<input type="search" placeholder="Search" id="site-search" name="q"
+       aria-label="Search"></input>
+      <button type="submit" id='searhButton' form="searchBar"></button>
+
+
+
+</form>
       </div>
     </>
   );
