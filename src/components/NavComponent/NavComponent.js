1735f7931959cfb09c39af9cbef20cc9c86e650c
Desislava Todorova <desi_14_94@abv.bg>
2022-02-02 17:05:01 +0200

    navComponent activeLink

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -1,11 +1,12 @@
 import { Link, Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useContext } from "react/cjs/react.development";
 import UserContext from "../../Contexts/UserContext";
 import "./NavComponent.css";
 import logo from "./static/Exclusion 1.png";
 const NavComponent = () => {
   const { token } = useContext(UserContext);
-
+const currentPath = useHistory().location.pathname.toLowerCase()
   if (!token) {
     return <Redirect to="/login" />;
   }
@@ -14,13 +15,13 @@ const NavComponent = () => {
       <img className="logoImage" src={logo} alt="logo" />
       <nav className="navList">
         <ul className="uLNav">
-          <li>
-            <Link to="/catalog" className="liItemLib">
+          <li className={currentPath.includes('catalog') ? "navl": ''}>
+            <Link to="/catalog" className='liItemLib'>
               Library
             </Link>
           </li>
-          <li>
-            <Link to="/settings" className="liItemSettings">
+          <li className={currentPath.includes('settings') ? "navl": ''}>
+            <Link to="/settings" className='liItemSettings'>
               settings
             </Link>
           </li>
